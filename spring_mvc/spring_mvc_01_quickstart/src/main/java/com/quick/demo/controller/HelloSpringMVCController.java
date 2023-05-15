1f1c9b884a7df3401e9b934bb96367c7392f9c5a
weijianhuawen <weijianhuawen@163.com>
2023-05-16 01:03:03 +0800

    spring mvc更新 快速开发mvc

--- spring_mvc/spring_mvc_01_quickstart/src/main/java/com/quick/demo/controller/HelloSpringMVCController.java
+++ spring_mvc/spring_mvc_01_quickstart/src/main/java/com/quick/demo/controller/HelloSpringMVCController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+
 @Controller
 public class HelloSpringMVCController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
         System.out.println("你好！Spring， MVC！");
+        HashMap<Integer,Integer> hashMap;
         return "hello spring mvc!";
     }
 }
