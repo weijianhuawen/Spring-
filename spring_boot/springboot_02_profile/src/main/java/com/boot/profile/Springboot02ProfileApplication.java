52096561239016c8e7716730632d4def2f0286fa
weijianhuawen <weijianhuawen@163.com>
2023-05-19 23:32:37 +0800

    spring boot 配置文件代码更新

---
+++ spring_boot/springboot_02_profile/src/main/java/com/boot/profile/Springboot02ProfileApplication.java
@@ -0,0 +1,13 @@
+package com.boot.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02ProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02ProfileApplication.class, args);
+	}
+
+}
