5772146d61eb14fbd418a931dcce8c6946b30586
weijianhuawen <weijianhuawen@163.com>
2023-05-19 23:33:04 +0800

    spring boot 配置文件加载优先级代码更新

---
+++ spring_boot/springboot_03_profile_priority/src/main/java/com/boot/profile/priority/Springboot03ProfilePriorityApplication.java
@@ -0,0 +1,13 @@
+package com.boot.profile.priority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03ProfilePriorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03ProfilePriorityApplication.class, args);
+    }
+
+}
---
+++ spring_boot/springboot_03_profile_priority/src/test/java/com/boot/profile/priority/Springboot03ProfilePriorityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.profile.priority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ProfilePriorityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
